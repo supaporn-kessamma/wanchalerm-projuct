5b81d038c68ebbe17ec250f21ca9d6cb58171fcc
supaporn-kessamma <78304939+supaporn-kessamma@users.noreply.github.com>
2022-03-06 00:01:00 +0700

    feat: project

---
+++ pages/cart.vue
@@ -0,0 +1,163 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <h2>ตะกร้าสินค้า</h2>
+    </v-col>
+    <v-col cols="9">
+      <v-data-table
+        :headers="headers"
+        :items="items"
+        class="elevation-1"
+        hide-default-footer
+      >
+        <template v-slot:item.image="{ item }">
+          <v-img
+            :src="item.image"
+            max-height="100"
+            max-width="100"
+            class="my-2"
+          />
+        </template>
+        <template v-slot:item.price="{ item }"> ฿{{ item.price }} </template>
+        <template v-slot:item.amout="{ item }">
+          <v-icon
+            :disabled="!item.amout"
+            class="grey lighten-2 pa-1"
+            @click="minus(item)"
+          >
+            mdi-minus
+          </v-icon>
+          <div class="d-inline grey lighten-4 rounded-md px-8 py-2 mx-2">
+            {{ item.amout }}
+          </div>
+          <v-icon class="grey lighten-2 pa-1" @click="plus(item)">
+            mdi-plus
+          </v-icon>
+        </template>
+        <template v-slot:item.total="{ item }"> ฿{{ item.total }} </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+            @click="deleteItem(item)"
+            color="white"
+            class="red rounded-circle pa-1"
+          >
+            mdi-delete
+          </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        </template>
+      </v-data-table>
+    </v-col>
+    <v-col cols="3">
+      <v-card outlined tile class="pa-2">
+        <v-row>
+          <v-col cols="12">
+            <h2>ที่อยู่จัดส่ง</h2>
+          </v-col>
+          <v-col cols="12">
+            <span>ชื่อ - นามสกุล</span>
+          </v-col>
+          <v-col cols="12" class="pt-0">
+            <v-text-field outlined dense hide-details />
+          </v-col>
+          <v-col cols="12" class="pt-0">
+            <span>ที่อยู่</span>
+          </v-col>
+          <v-col cols="12" class="pt-0">
+            <v-text-field outlined dense hide-details />
+          </v-col>
+          <v-col cols="6" class="pt-0">
+            <span>เมือง</span>
+          </v-col>
+          <v-col cols="6" class="pt-0">
+            <span>จังหวัด</span>
+          </v-col>
+          <v-col cols="6" class="pt-0">
+            <v-text-field outlined dense hide-details />
+          </v-col>
+          <v-col cols="6" class="pt-0">
+            <v-text-field outlined dense hide-details />
+          </v-col>
+          <v-col cols="6" class="pt-0">
+            <span>ประเทศ</span>
+          </v-col>
+          <v-col cols="6" class="pt-0">
+            <span>รหัสไปรษณีย์</span>
+          </v-col>
+          <v-col cols="6" class="pt-0">
+            <v-text-field outlined dense hide-details />
+          </v-col>
+          <v-col cols="6" class="pt-0">
+            <v-text-field outlined dense hide-details />
+          </v-col>
+          <v-col cols="12" class="pt-0">
+            <span>เบอร์โทรศัพท์</span>
+          </v-col>
+          <v-col cols="12" class="pt-0">
+            <v-text-field outlined dense hide-details />
+          </v-col>
+          <v-col cols="12">
+            <v-btn block color="green" class="white--text">สั่งสินค้า</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        { text: "รายการ", value: "image", width: 100, align: "center" },
+        { text: "", value: "orders", align: "start" },
+        { text: "ราคา", value: "price", align: "center" },
+        { text: "จำนวน", value: "amout", align: "center" },
+        { text: "ราคารวม", value: "total", align: "center" },
+        { text: "ลบ", value: "actions", sortable: false },
+      ],
+      items: [
+        {
+          image:
+            "https://www.otoptoday.com/images/upload_img/products/otop_img_11568805905.jpg",
+          orders: "ส้มแขก",
+          price: 100,
+          amout: 1,
+          total: 100,
+        },
+      ],
+      min: 0,
+    };
+  },
+  watch: {
+    "item.amout": {
+      handler() {
+        console.log("item.amout");
+        item.total = item.amout * item.total;
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    plus(item) {
+      if (item.amout >= this.min) {
+        const num = item.amout;
+        item.amout = num + 1;
+
+        item.total = item.price * item.amout;
+      }
+    },
+    minus(item) {
+      if (item.amout > this.min) {
+        const num = item.amout;
+        item.amout = num - 1;
+
+        item.total = item.price * item.amout;
+      }
+    },
+    deleteItem(item) {},
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="12" md="12">
+      <h2 class="">สินค้าทั้งหมด</h2>
+    </v-col>
+    <v-col cols="9">
+      <v-row>
+        <v-col cols="4" v-for="(product, idx) in products" :key="idx">
+          <v-card elevation="2" max-width="230" class="pa-2">
+            <v-row align="center" justify="center">
+              <v-col cols="12">
+                <v-img :src="product.image" max-width="230" max-height="180" />
+              </v-col>
+              <v-col cols="12" class="pt-0">
+                <div class="d-flex justify-space-between">
+                  <div class="">{{ product.name }}</div>
+                  <v-card class="px-2 py-1" outlined tile>
+                    <div class="">฿{{ product.price }}</div>
+                  </v-card>
+                </div>
+              </v-col>
+              <v-col cols="12" class="d-flex justify-end">
+                <v-card
+                  class="px-2 py-1"
+                  outlined
+                  tile
+                  @click="addProduct(product)"
+                >
+                  <div>เพิ่มลงในตะกร้า</div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-col cols="3">
+      <v-row>
+        <v-col cols="12">
+          <div class="text-center">ประเภทสินค้า</div>
+        </v-col>
+        <v-col cols="12">
+          <v-btn block>เครื่องดื่ม</v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn block>ยา</v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn block>อาหาร</v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn block>สมุนไพร</v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn block>เครื่องดื่ม(ไม่มีแอลกอฮอล์)</v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn block>เสื้อผ้า</v-btn>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      length: 0,
+      products: [
+        {
+          image:
+            "https://www.otoptoday.com/images/upload_img/products/otop_img_11568805905.jpg",
+          name: "ส้มแขก",
+          price: 100,
+          amout: 1,
+          total: 100,
+        },
+        {
+          image:
+            "https://www.otoptoday.com/images/upload_img/products/otop_img_11568805905.jpg",
+          name: "ส้มแขก",
+          price: 100,
+          amout: 1,
+          total: 100,
+        },
+        {
+          image:
+            "https://www.otoptoday.com/images/upload_img/products/otop_img_11568805905.jpg",
+          name: "ส้มแขก",
+          price: 100,
+          amout: 1,
+          total: 100,
+        },
+        {
+          image:
+            "https://www.otoptoday.com/images/upload_img/products/otop_img_11568805905.jpg",
+          name: "ส้มแขก",
+          price: 100,
+          amout: 1,
+          total: 100,
+        },
+      ],
+    };
+  },
+  methods: {
+    addProduct(item) {
+      console.log(item);
+    },
+  },
+};
+</script>
